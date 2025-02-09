79cd272b4211762ad57c4a912425659080161ccb
rahultudu-h <rahultudu4705@gmail.com>
2025-02-09 15:55:19 +0530

    add exp15

---
+++ exp15/wap15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()	{
+	int num1,num2;
+	int counter;
+	printf("Enter the first number: ");
+	scanf("%d",&num1);
+
+	printf("Enter the second number: ");
+	scanf("%d",&num2);
+	printf("The numbers between %d and %d are: ",num1,num2);
+	for (counter = num1+1; (counter) < num2; counter++){
+		printf("%d ",counter);
+	}	
+	printf("\n");
+
+
+
+
+
+	return 0;
+}
